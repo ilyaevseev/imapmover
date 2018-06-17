c3a4b9254fd7c5c1303c27438940ef615cd3731a
Ilya Evseev <ilya.evseev@gmail.com>
2018-06-18 02:27:18 +0300

    Second_commit.

--- imapmover.py
+++ imapmover.py
@@ -13,19 +13,27 @@ class MailSession:
 
     session = None
 
-    def __init__(self, account_name, account):
-        pprint(account)
+    def __init__(self, account):
         self.session = imaplib.IMAP4_SSL(account['host'])
-        rv, data = self.session.login(account['user'], account['pass'])
-        pprint(rv)
-        pprint(data)
-        return
+        self.session.login(account['user'], account['pass'])
 
-    def read_folder(folder_name = None):
-        return
+    def read_folder(self, folder_name = 'INBOX'):
+        #pprint(self.session.list())  # ..rv, folders
+        self.session.select(folder_name)
+        rv, msgids = self.session.search(None, 'ALL')
+        return msgids[0].split()  #..string containing numeric ids => return as array
 
-    def move_msg(msg, dest_folder):
-        return
+    def try_rule(self, msgid, rule):
+        """
+        Try to match message against rule.
+        On success, move message to IMAP folder selected by rule, and return True.
+        Otherwise return False.
+        """
+
+        print("msgid = %s, name = %s, rule = %s, dest = %s"
+                % ( msgid, rule['name'], rule['match_rule'], rule['dest_folder'] ))
+        # ???
+        return True
 
 class ImapMover:
 
@@ -35,22 +43,36 @@ class ImapMover:
     def read_section(self, cfgfile, cfg, section_name, required_fields):
 
         if not section_name in cfg:
-            raise ValueError("missing \"%s\" in %s" % ( section_name, cfgfile ))
+            raise LookupError("missing \"%s\" in %s" % ( section_name, cfgfile ))
 
-        data = cfg[section_name]
+        records = cfg[section_name]
 
-        if not data:
-            raise ValueError("empty \"%s\" in %s" % ( section_name, cfgfile ))
+        if not records:
+            raise LookupError("empty \"%s\" in %s" % ( section_name, cfgfile ))
 
-        records = data.items()
+        if not isinstance(records, dict):
+            raise TypeError("wrong section \"%s\" in \"%s\""
+                              % ( section_name, cfgfile ))
 
-        for record_name, record in records:
+        result = []
+
+        for record_name in sorted(records):
+
+            record = records[record_name]
+
+            if not isinstance(record, dict):
+                raise TypeError("wrong record \"%s\" in section \"%s\" in \"%s\""
+                                  % ( record_name, section_name, cfgfile ))
+
             for field in required_fields:
                 if not field in record:
-                    raise ValueError( "missing \"%s\" in record \"%s\" in section \"%s\" in %s"
+                    raise LookupError( "missing \"%s\" in record \"%s\" in section \"%s\" in %s"
                                   % ( field, record_name, section_name, cfgfile ))
 
-        return records
+            record['name'] = record_name
+            result.append(record)
+
+        return result
 
     def read_config(self, cfgfile):
 
@@ -60,21 +82,15 @@ class ImapMover:
         self.mail_accounts = self.read_section(cfgfile, cfg, 'mail_accounts', ['host','user','pass'      ])
         self.filter_rules  = self.read_section(cfgfile, cfg, 'filter_rules' , ['match_rule','dest_folder'])
 
-    def match_rule(self, msg, rule):
-
-        # ???
-        return
-
     def run(self):
 
-        for account_name, account in self.mail_accounts:
-            session = MailSession(account_name, account)
-            return
-            msgs = session.read_folder()
-            for msg in msgs:
-                for rule_name, rule in self.filter_rules:
-                    if self.match_rule(msg, rule):
-                        session.move_msg(msg, rule.dest_folder)
+        for account in self.mail_accounts:
+            session = MailSession(account)
+            msgids = session.read_folder()
+            for msgid in msgids:
+                for rule in self.filter_rules:
+                    if session.try_rule(msgid, rule):
+                        break  #..skip remaining rules
 
 def main():
     mover = ImapMover()
