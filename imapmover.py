b2cb8b867914cf422b1caa2727082ad5d65791a4
Ilya Evseev <ilya.evseev@gmail.com>
2018-07-02 04:20:28 +0300

    Logging added; get_destination fixed.

--- imapmover.py
+++ imapmover.py
@@ -12,6 +12,25 @@ import email
 import email.header
 import email.utils
 
+class Log:
+
+    __level = -1
+
+    @staticmethod
+    def init_loglevel():
+        if Log.__level >= 0: return
+        Log.__level = 0
+        if not 'VERBOSE' in os.environ: return
+        val = os.environ['VERBOSE']
+        if not val.isdigit(): return
+        Log.__level = val
+
+    @staticmethod
+    def verbose(level, msg):
+        Log.init_loglevel()
+        if level > Log.__level: return
+        print("<%d> %s" % (level, msg))
+
 class Destination:
     pass
 
@@ -28,19 +47,28 @@ class LocalDestination(Destination):
         return os.path.join(self.topdir, p)
 
     def mkdir(self, dirpath):
+        p = self.path2full(dirpath)
         try:
-            os.makedirs(self.path2full(dirpath))
+            os.makedirs(p)
+            Log.verbose(2, "Created local directory: " + p)
         except OSError as e:
-            if e.errno != errno.EEXIST: raise
+            if e.errno != errno.EEXIST:
+                raise
+            Log.verbose(4, "Local directory already exist: " + p)
 
     def putfile(self, filepath, data):
 
         p = self.path2full(filepath)
-        if os.path.exists(p): return False
 
+        if os.path.exists(p):
+            Log.verbose(2, "Local file already exist, skipped: " + filepath)
+            return False
+
+        Log.verbose(2, "Write local file: " + p)
         f = open(p, 'wb')
         f.write(data)
         f.close
+        return True
 
 class DropboxDestination(Destination):
 
@@ -62,24 +90,32 @@ class MailSession:
     MASKLEN = 5
 
     def __init__(self, account):
+        Log.verbose(1, "Connect to IMAP server: " + account['host'])
         self.session = imaplib.IMAP4_SSL(account['host'])
         self.session.login(account['user'], account['pass'])
+        Log.verbose(3, "Connect to IMAP server: success")
 
     def read_folder(self, folder_name = 'INBOX'):
+
+        Log.verbose(3, "Select IMAP folder: " + folder_name)
         #pprint(self.session.list())  # ..rv, folders
         self.session.select(folder_name)
 
+        Log.verbose(3, "Search unseen messages")
         rv, msgids = self.session.search(None, '(UNSEEN)')
         msgids = msgids[0].split()  #..string containing numeric ids => return as array
         msgs = []
 
+        Log.verbose(3, "Fetch %d message headers" % (len(msgids)))
         for msgid in msgids:
+            Log.verbose(5, "Fetch message header: " + str(msgid))
             rv,data = self.session.fetch(msgid, '(RFC822)')
             txt = data[0][1]
             msg = email.message_from_string(txt)
             msg.__setitem__('__MSGID__', msgid)
             msgs.append(msg);
 
+        Log.verbose(4, "Fetch message headers: done")
         return msgs
 
     def email_addr_match(self, actual, needed):
@@ -137,13 +173,19 @@ class MailSession:
 
     def get_destination(self, rule, destinations):
 
-        if not destinations or not 'destination' in rule:
+        if not 'destination' in rule:
+            Log.verbose(6, "Use local destination")
             return LocalDestination()
 
         destname = rule['destination']
-        if not destname in destinations:
-            raise LookupError("rule contains unknown destination \"%s\"" % (destname))
 
+        if not destinations:
+            raise LookupError("rule uses destination %s, but no destinations defined" % (destname))
+
+        if not destname in destinations:
+            raise LookupError("rule uses unknown destination \"%s\"" % (destname))
+
+        Log.verbose(6, "Use destination: " + destname)
         return destinations[destname]
 
     def try_rule(self, msg, rule, destinations):
@@ -159,7 +201,10 @@ class MailSession:
         if not self.any_attachment_match(msg, rule['suffix'], rule['mask']):
             return False
 
-        #print '%s - %s => %s' % ( msg['From'], msg['Subject'], rule['name'] )
+        Log.verbose(1, "Fetch message \"%s\" from \"%s\" by rule \"%s\"" %
+                      ( msg['Subject'] if msg['Subject'] else 'NOSUBJ',
+                        msg['From'   ] if msg['From'   ] else 'NOFROM',
+                        rule['name'] ) )
 
         msgdir = rule['dest_folder'] if 'dest_folder' in rule else self.build_dirname(msg['Subject'], rule['mask'])
 
@@ -181,6 +226,7 @@ class MailSession:
 
             dest.putfile(os.path.join(msgdir, filename), data)
 
+        Log.verbose(3, "Set flag to message: " + msg['__MSGID__'])
         self.session.store(msg['__MSGID__'], '+FLAGS', '\\Flagged')
 
         return True
@@ -259,12 +305,14 @@ class ImapMover:
 
     def read_config(self, cfgfile):
 
+        Log.verbose(1, "Read config from " + cfgfile)
         fd = open(cfgfile, 'r')
         cfg = yaml.load(fd)
 
         self.mail_accounts = self.read_section(cfgfile, cfg, 'mail_accounts', ['host','user','pass'])
         self.filter_rules  = self.read_section(cfgfile, cfg, 'filter_rules' , ['from','suffix','mask','dest_folder'])
         self.destinations  = self.read_destinations(cfgfile, cfg)
+        Log.verbose(3, "Read config from " + cfgfile + ": done.")
 
     def run(self):
 
@@ -281,6 +329,7 @@ def main():
     fname = sys.argv[1] if len(sys.argv) > 1 else os.path.splitext(os.path.basename(sys.argv[0]))[0] + '.yml'
     mover.read_config(fname)
     mover.run()
+    Log.verbose(1, "Done.")
 
 if __name__ == "__main__":
     main()
